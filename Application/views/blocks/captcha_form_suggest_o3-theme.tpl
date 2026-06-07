3194796aa15084e9c75d64b47120421d885c7770
Steffen Winde <inserv@winde-ganzig.de>
2026-06-07 11:02:25 +0200

    update captcha forms to use 'o3-theme' and create new template files

---
+++ Application/views/blocks/captcha_form_suggest_o3-theme.tpl
@@ -0,0 +1,3 @@
+[{$smarty.block.parent}]
+
+[{include file="oecaptcha_wave.tpl" labelCssClass="control-label col-md-3" inputCssClass="col-md-9"}]
--- metadata.php
+++ metadata.php
@@ -120,44 +120,44 @@ $aModule = [
         [
             'template' => 'form/contact.tpl',
             'block'=>'captcha_form',
-            'theme' => 'wave',
-            'file'=>'Application/views/blocks/captcha_form_contact_wave.tpl'
+            'theme' => 'o3-theme',
+            'file'=>'Application/views/blocks/captcha_form_contact_o3-theme.tpl'
         ],
         [
             'template' => 'form/newsletter.tpl',
             'block'=>'captcha_form',
-            'theme' => 'wave',
-            'file'=>'Application/views/blocks/captcha_form_newsletter_wave.tpl'
+            'theme' => 'o3-theme',
+            'file'=>'Application/views/blocks/captcha_form_newsletter_o3-theme.tpl'
         ],
         [
             'template' => 'form/privatesales/invite.tpl',
-            'theme' => 'wave',
+            'theme' => 'o3-theme',
             'block'=>'captcha_form',
-            'file'=>'Application/views/blocks/captcha_form_invite_wave.tpl'
+            'file'=>'Application/views/blocks/captcha_form_invite_o3-theme.tpl'
         ],
         [
             'template' => 'form/pricealarm.tpl',
             'block'=>'captcha_form',
-            'theme' => 'wave',
-            'file'=>'Application/views/blocks/captcha_form_pricealarm_wave.tpl'
+            'theme' => 'o3-theme',
+            'file'=>'Application/views/blocks/captcha_form_pricealarm_o3-theme.tpl'
         ],
         [
             'template' => 'form/suggest.tpl',
             'block'=>'captcha_form',
-            'theme' => 'wave',
-            'file'=>'Application/views/blocks/captcha_form_suggest_wave.tpl'
+            'theme' => 'o3-theme',
+            'file'=>'Application/views/blocks/captcha_form_suggest_o3-theme.tpl'
         ],
         [
             'template' => 'form/forgotpwd_email.tpl',
             'block'=>'captcha_form',
-            'theme' => 'wave',
-            'file'=>'Application/views/blocks/captcha_form_forgotpwd_wave.tpl'
+            'theme' => 'o3-theme',
+            'file'=>'Application/views/blocks/captcha_form_forgotpwd_o3-theme.tpl'
         ],
         [
             'template' => 'form/fieldset/user_billing.tpl',
             'block'=>'captcha_form',
-            'theme' => 'wave',
-            'file'=>'Application/views/blocks/captcha_form_user_billing_wave.tpl'
+            'theme' => 'o3-theme',
+            'file'=>'Application/views/blocks/captcha_form_user_billing_o3-theme.tpl'
         ],
     ],
     'events'       => [
